9e8b2624a879aa3553c032e503fc8d5a18e158fd
Kashkarov Andrei <kashrr@rambler.ru>
2024-05-10 14:02:31 +0300

    creat funkcion saize array

--- Program.cs
+++ Program.cs
@@ -1,2 +1,17 @@
 ﻿// Итоговая контрольная работа по основному блоку
 string[] array1 = new string[6] { "257", "45", "hello", "world", "res", "456" };
+
+
+int SaizArray(string[] array1)
+{
+    int n = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+        if (array1[i].Length <= 3)
+        {
+            n++;
+        }
+    }
+    return n;
+}
+
